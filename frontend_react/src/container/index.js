376c9a3ee086ed0e82787c043dcbe62283bcfa8a
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2022-04-05 19:19:16 -0400

    added sanity and connected base components and stylesheets

---
+++ frontend_react/src/container/index.js
@@ -0,0 +1,8 @@
+import Footer from './Footer/Footer';
+import Header from './Header/Header';
+import Skills from './Skills/Skills';
+import Testimonial from './Testimonial/Testimonial';
+import Work from './Work/Work';
+import About from './About/About';
+
+export { About, Footer, Header, Skills, Testimonial, Work };
