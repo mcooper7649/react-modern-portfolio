d7b7a364ad124d9de646285a11d81880a6f4e711
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2022-04-06 13:34:28 -0400

    added motion div and main divv to work container

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -1,9 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { AiFillEye, AiFillGithub } from 'react-icons/ai';
+import { motion } from 'framer-motion';
+import { AppWrap } from '../../wrapper/AppWrap';
+import { urlFor, client } from '../../client';
 
 import './Work.scss';
 
 const Work = () => {
-  return <div>Work</div>;
+  const [activeFilter, setActiveFilter] = useState('All');
+  const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
+  const [works, setWorks] = useState([]);
+  const [filterWork, setFilterWork] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "works" ]';
+    client.fetch(query).then((data) => {
+      setWorks(data);
+      setFilterWork(data);
+    });
+  }, []);
+
+  const handleWorkFilter = (item) => {};
+
+  return (
+    <>
+      <h2 className="head-text">
+        My Creative <span>Porfolio </span>Section
+      </h2>
+      <div className="app__work-filter">
+        {['UI/UX', 'Web App', 'Mobile App', 'Reactjs', 'All'].map(
+          (item, index) => (
+            <div
+              key={index}
+              onClick={() => handleWorkFilter(item)}
+              className={`app__work-filter-item app__flex p-text ${
+                activeFilter === item ? 'item-active' : ''
+              }`}
+            >
+              {item}
+            </div>
+          )
+        )}
+      </div>
+      <motion.div
+        animate={animateCard}
+        transitions={{ duration: 0.5, delayChildren: 0.5 }}
+        className="app_portfolio"
+      ></motion.div>
+    </>
+  );
 };
 
 export default Work;
