c6e89fc063c630ec3b649fbf226122b35c5dc117
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2022-04-09 23:59:24 -0400

    updated to new logo and changed navbar img scss

--- frontend_react/src/constants/images.js
+++ frontend_react/src/constants/images.js
@@ -27,6 +27,8 @@ import about04 from '../assets/about04.png';
 import profile from '../assets/profile.png';
 import circle from '../assets/circle.svg';
 import logo from '../assets/logo.png';
+import newlogo from '../assets/newlogo.png';
+import ninjalogo from '../assets/ninja-logo.png';
 
 import adidas from '../assets/adidas.png';
 import amazon from '../assets/amazon.png';
@@ -63,6 +65,8 @@ export default {
   profile,
   circle,
   logo,
+  newlogo,
+  ninjalogo,
   adidas,
   amazon,
   asus,
